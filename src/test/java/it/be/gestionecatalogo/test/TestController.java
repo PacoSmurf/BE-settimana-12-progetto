1a094a3a549bbfd031147845a2173c94e2913b0d
torca <torca@Paco>
2022-03-06 22:21:34 +0100

    .

--- src/test/java/it/be/gestionecatalogo/test/TestController.java
+++ src/test/java/it/be/gestionecatalogo/test/TestController.java
@@ -7,13 +7,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.test.context.support.WithAnonymousUser;
-import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
 import it.be.gestionecatalogo.controller.LibroController;
-import it.be.gestionecatalogo.model.Libro;
 
 @SpringBootTest
 
